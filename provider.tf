1d5a233b1840c3df429a04ff0ba3e742d34ce1cd
Amitish <58432032+Amitish@users.noreply.github.com>
2025-07-02 15:36:25 +0530

    2 comments new ad kiye hai (#1)
    * 2 comments new ad kiye hai
    
    * last phase
    
    ---------
    
    Co-authored-by: Amit Bisht <amit.bisht@dishtv.in>

--- provider.tf
+++ provider.tf
@@ -10,5 +10,12 @@ terraform {
 provider "azurerm" {
   # Configuration options
   features {}
-  subscription_id = ""
+  subscription_id = "81c934f4-2881-4075-83be-37c3831d4469"
 }
+
+# Iss code mei maine kuch new changes kiye hashicorp
+# ab dekhte hai kya hota hai
+
+# Kya bhai kuch hua he nhi 
+# dekho kya hota hai
+# shayad ab ho jaye
