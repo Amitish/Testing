37e6609de50c8c3e6d5648d539dbaa92e8931a93
Amit Bisht <amit.bisht@dishtv.in>
2025-07-02 08:03:40 +0530

    2 comments new ad kiye hai

--- provider.tf
+++ provider.tf
@@ -10,5 +10,8 @@ terraform {
 provider "azurerm" {
   # Configuration options
   features {}
-  subscription_id = ""
+  subscription_id = "81c934f4-2881-4075-83be-37c3831d4469"
 }
+
+# Iss code mei maine kuch new changes kiye hashicorp
+# ab dekhte hai kya hota hai
